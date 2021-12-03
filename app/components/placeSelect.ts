2a2b388c190fc628be15580fd7afc8381263594f
waylogger <waylogger@mail.ru>
2021-12-03 17:01:08 +0300

    fix addresses

--- app/components/placeSelect.ts
+++ app/components/placeSelect.ts
@@ -1,66 +1,105 @@
 /**
  * @module placeSelect.ts
  * @description генерирует разметку для выбора места получения и возврата авто
-*/
+ */
 
 import { PlacesResponse } from "../CORS/entities/apiExchange/serverTypes";
 import { option } from "../shared/sharedActions";
-import * as shared from '../shared/sharedData';
-import $ from 'jquery'
-import { State } from '../state/state';
+import * as shared from "../shared/sharedData";
+import $ from "jquery";
+import { State } from "../state/state";
 
 export function placeOptions(state: State): string {
-	let resStr: string = '';
-	state.getPlacesForReceiveAndReturnCars().places.forEach(
-		(place) => resStr += option(`${place.title} + ${place.delivery_cost} ₽`)
-	);
-	$(`#${shared.domElementId.returnPlaceSelectId}`).html(resStr);
-	$(`#${shared.domElementId.receivePlaceSelectId}`).html(resStr);
-	return resStr;
+  let resStr: string = "";
+  state
+    .getPlacesForReceiveAndReturnCars()
+    .places.filter((el) => el.title)
+    .forEach(
+      (place) => (resStr += option(`${place.title} + ${place.delivery_cost} ₽`))
+    );
+  $(`#${shared.domElementId.returnPlaceSelectId}`).html(resStr);
+  $(`#${shared.domElementId.receivePlaceSelectId}`).html(resStr);
+  return resStr;
 }
 
 export const selectPlace = (state: State): void => {
-	$(`#${shared.domElementId.receivePlaceSelectId}`).on(
-		'change',
-		() => {
-			let txt: any = $(`#${shared.domElementId.receivePlaceSelectId}`).val()?.toString().trim().split(' + ')[0];
-			const customInx = state.getPlacesForReceiveAndReturnCars().places.findIndex((item)=>item.title.match(/ДРУГОЕ/));
-			if (txt === state.getPlacesForReceiveAndReturnCars().places[customInx].title) {
-				$(`#${shared.domElementId.receiveCustomPlaceInputId}`).removeClass('customPlace-hidden');
-				$(`#${shared.domElementId.receiveCustomPlaceId}`).removeClass('customPlace-hidden')
-				$(`#${shared.domElementId.receiveCustomPlaceId}`).removeClass('customPlace-wrap-start')
-				$(`#${shared.domElementId.receiveCustomPlaceId}`).addClass('customPlace-visible')
-			} else {
-				$(`#${shared.domElementId.receiveCustomPlaceInputId}`).addClass('customPlace-hidden');
-				$(`#${shared.domElementId.receiveCustomPlaceId}`).addClass('customPlace-hidden')
-				$(`#${shared.domElementId.receiveCustomPlaceId}`).addClass('customPlace-wrap-start')
-				$(`#${shared.domElementId.receiveCustomPlaceId}`).removeClass('customPlace-visible')
-			}
-		}
-	);
+  $(`#${shared.domElementId.receivePlaceSelectId}`).on("change", () => {
+    let txt: any = $(`#${shared.domElementId.receivePlaceSelectId}`)
+      .val()
+      ?.toString()
+      .trim()
+      .split(" + ")[0];
+    const customInx = state
+      .getPlacesForReceiveAndReturnCars()
+      .places.findIndex((item) => item.title.match(/Другое/));
+    if (
+      txt === state.getPlacesForReceiveAndReturnCars().places[customInx].title
+    ) {
+      $(`#${shared.domElementId.receiveCustomPlaceInputId}`).removeClass(
+        "customPlace-hidden"
+      );
+      $(`#${shared.domElementId.receiveCustomPlaceId}`).removeClass(
+        "customPlace-hidden"
+      );
+      $(`#${shared.domElementId.receiveCustomPlaceId}`).removeClass(
+        "customPlace-wrap-start"
+      );
+      $(`#${shared.domElementId.receiveCustomPlaceId}`).addClass(
+        "customPlace-visible"
+      );
+    } else {
+      $(`#${shared.domElementId.receiveCustomPlaceInputId}`).addClass(
+        "customPlace-hidden"
+      );
+      $(`#${shared.domElementId.receiveCustomPlaceId}`).addClass(
+        "customPlace-hidden"
+      );
+      $(`#${shared.domElementId.receiveCustomPlaceId}`).addClass(
+        "customPlace-wrap-start"
+      );
+      $(`#${shared.domElementId.receiveCustomPlaceId}`).removeClass(
+        "customPlace-visible"
+      );
+    }
+  });
 
-	$(`#${shared.domElementId.returnPlaceSelectId}`).on(
-		'change',
-		() => {
-			let txt: any = $(`#${shared.domElementId.returnPlaceSelectId}`).val()?.toString().trim().split(' + ')[0];
-			const customInx = state.getPlacesForReceiveAndReturnCars().places.findIndex((item)=>item.title.match(/ДРУГОЕ/));
-			if (txt === state.getPlacesForReceiveAndReturnCars().places[customInx].title) {
-				$(`#${shared.domElementId.returnCustomPlaceInputId}`).removeClass('customPlace-hidden');
-				$(`#${shared.domElementId.returnCustomPlaceId}`).removeClass('customPlace-hidden')
-				$(`#${shared.domElementId.returnCustomPlaceId}`).removeClass('customPlace-wrap-end')
-				$(`#${shared.domElementId.returnCustomPlaceId}`).addClass('customPlace-visible')
-			} else {
-				$(`#${shared.domElementId.returnCustomPlaceInputId}`).addClass('customPlace-hidden');
-				$(`#${shared.domElementId.returnCustomPlaceId}`).addClass('customPlace-hidden')
-				$(`#${shared.domElementId.returnCustomPlaceId}`).addClass('customPlace-wrap-end')
-				$(`#${shared.domElementId.returnCustomPlaceId}`).removeClass('customPlace-visible')
-			}
-		}
-	);
-}
-
-
-
-
-
-
+  $(`#${shared.domElementId.returnPlaceSelectId}`).on("change", () => {
+    let txt: any = $(`#${shared.domElementId.returnPlaceSelectId}`)
+      .val()
+      ?.toString()
+      .trim()
+      .split(" + ")[0];
+    const customInx = state
+      .getPlacesForReceiveAndReturnCars()
+      .places.findIndex((item) => item.title.match(/Другое/));
+    if (
+      txt === state.getPlacesForReceiveAndReturnCars().places[customInx].title
+    ) {
+      $(`#${shared.domElementId.returnCustomPlaceInputId}`).removeClass(
+        "customPlace-hidden"
+      );
+      $(`#${shared.domElementId.returnCustomPlaceId}`).removeClass(
+        "customPlace-hidden"
+      );
+      $(`#${shared.domElementId.returnCustomPlaceId}`).removeClass(
+        "customPlace-wrap-end"
+      );
+      $(`#${shared.domElementId.returnCustomPlaceId}`).addClass(
+        "customPlace-visible"
+      );
+    } else {
+      $(`#${shared.domElementId.returnCustomPlaceInputId}`).addClass(
+        "customPlace-hidden"
+      );
+      $(`#${shared.domElementId.returnCustomPlaceId}`).addClass(
+        "customPlace-hidden"
+      );
+      $(`#${shared.domElementId.returnCustomPlaceId}`).addClass(
+        "customPlace-wrap-end"
+      );
+      $(`#${shared.domElementId.returnCustomPlaceId}`).removeClass(
+        "customPlace-visible"
+      );
+    }
+  });
+};
--- app/state/state.ts
+++ app/state/state.ts
@@ -506,7 +506,20 @@ export class State {
 
   public getPlacesForReceiveAndReturnCars(): PlacesResponse {
     const places = this.placesForReceiveAndReturnCars;
-    return { result_code: places.result_code, places: places.places };
+    const correctedPlaces = places.places;
+    correctedPlaces[0].title = "Офис, ул. Гриса Плиева 5Б";
+    correctedPlaces[1].title = "Аэропорт Владикавказ";
+    correctedPlaces[2].title = "Фиагдон";
+    correctedPlaces[3].title = "Другое место (Владикавказ)";
+    correctedPlaces[4].title = "Аэропорт Беслан";
+    correctedPlaces[5].title = "";
+
+	// меняем фиагдон и АЭ Беслан местами
+    const store = correctedPlaces[2];
+    correctedPlaces[2] = correctedPlaces[4];
+    correctedPlaces[4] = store;
+
+    return { result_code: places.result_code, places: correctedPlaces };
   }
 
   public saveCustomersPhone(num: string | undefined): void {
@@ -590,7 +603,6 @@ export class State {
     today.setSeconds(-1);
 
     if (isBefore(dt, today) && !isEqual(dt, today)) {
-
       return dateIsBusy;
     }
     if (this.isSecondDateOfRangeWasSelect()) {
